278868cfeccf53524eef918a1786183841e322a5
Yuta Uchida <yuchida4dev@gmail.com>
2025-01-30 17:10:07 +0900

    add more data

--- app/index.tsx
+++ app/index.tsx
@@ -31,6 +31,24 @@ const items: Item[] = [
     imageUrl: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tag: "city"
   },
+  {
+    title: "Sunset Overdrive",
+    content: "A breathtaking view of the sun setting over the ocean, painting the sky with hues of orange and purple.",
+    imageUrl: "https://plus.unsplash.com/premium_photo-1672116452571-896980a801c8?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    tag: "sunset-b"
+  },
+  {
+    title: "Mountain Expedition",
+    content: "An adventurous journey through rugged mountain trails, capturing the essence of nature's grandeur.",
+    imageUrl: "https://plus.unsplash.com/premium_photo-1680964717433-bc078a238170?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNlYXxlbnwwfHwwfHx8MA%3D%3D",
+    tag: "mountain-b"
+  },
+  {
+    title: "City Lights",
+    content: "A mesmerizing cityscape at night, with glowing skyscrapers reflecting off the river below.",
+    imageUrl: "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    tag: "city-b"
+  },
 ];
 
 
